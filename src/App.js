db4770a6f1b623ee6c38e3ff30bb52f538a54000
seanygberg <60160094+seanygberg@users.noreply.github.com>
2025-01-03 23:53:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import UserContext from './UserContext';
 import NavRoutes from './routes/NavRoutes.js';
 import useLocalStorage from "./hooks/useLocalStorage";
-import jwt_decode from 'jwt-decode';
+import * as jwt_decode from 'jwt-decode';
 import JoblyApi from "./api"
 import './App.css';
 
