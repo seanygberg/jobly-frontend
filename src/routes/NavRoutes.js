069c6cd05ae163e43d97733ec90c480f6d02b2d2
seanygberg <60160094+seanygberg@users.noreply.github.com>
2025-01-04 12:08:49 -0500

    first commit

--- src/routes/NavRoutes.js
+++ src/routes/NavRoutes.js
@@ -1,32 +1,57 @@
 import React from "react";
-import Home from "../home/Home.js"
-import LoginForm from "../user/LoginForm.js"
-import SignupForm from "../user/SignupForm.js"
-import ProfileForm from "../user/ProfileForm.js"
-import PrivateRoute from "./PrivateRoute.js"
 import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../home/Home.js";
+import LoginForm from "../user/LoginForm.js";
+import SignupForm from "../user/SignupForm.js";
+import ProfileForm from "../user/ProfileForm.js";
+import PrivateRoute from "./PrivateRoute.js";
 
 function NavRoutes({ signup, login }) {
-    /**
-     *          <Route path="/companies" element={<Companies />} />
-                <Route path="/companies/:handle" element={<CompanyDetail />} />
-                <Route path="/jobs" element={<Jobs />} />
-     */
     return (
         <div>
             <Routes>
+                {/* Public Routes */}
                 <Route path="/" element={<Home />} />
-                <PrivateRoute path="/companies" />
-                <PrivateRoute path="/companies/:handle" />
-                <PrivateRoute path="/jobs" />
-                <Route path="/login" element={<LoginForm login={login}/>} />
-                <Route path="/signup" element={<SignupForm signup={signup}/>} />
-                <Route path="/profile" element={<PrivateRoute> <ProfileForm /> </PrivateRoute>}/>
+                <Route path="/login" element={<LoginForm login={login} />} />
+                <Route path="/signup" element={<SignupForm signup={signup} />} />
+                
+                {/* Private Routes */}
+                <Route
+                    path="/profile"
+                    element={
+                        <PrivateRoute>
+                            <ProfileForm />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/companies"
+                    element={
+                        <PrivateRoute>
+                            <div>Companies Component Placeholder</div>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/companies/:handle"
+                    element={
+                        <PrivateRoute>
+                            <div>Company Detail Component Placeholder</div>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/jobs"
+                    element={
+                        <PrivateRoute>
+                            <div>Jobs Component Placeholder</div>
+                        </PrivateRoute>
+                    }
+                />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
-            
         </div>
     );
-};
+}
 
 export default NavRoutes;
